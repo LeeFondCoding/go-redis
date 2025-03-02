fbb4be6526e1efa7dbace5a7940f59e0a0a8f0ca
lichun <lichun0822@foxmail.com>
2025-03-02 13:51:53 +0800

    feat-"echo服务器与测试文件"

---
+++ tcp/echo.go
@@ -0,0 +1,73 @@
+package tcp
+
+import (
+	"bufio"
+	"context"
+	"goredis/lib/logger"
+	"goredis/lib/sync/atomic"
+	"goredis/lib/sync/wait"
+	"io"
+	"net"
+	"sync"
+	"time"
+)
+
+type EchoClient struct {
+	conn    net.Conn
+	waiting wait.Wait
+}
+
+func (c *EchoClient) Close() error {
+	c.waiting.WaitWithTimeout(10 * time.Second)
+	_ = c.conn.Close()
+	return nil
+}
+
+type EchoHandler struct {
+	activeConn sync.Map
+	closing    atomic.Boolean
+}
+
+func MakeEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+func (h *EchoHandler) Handle(ctx context.Context, conn net.Conn) {
+	if h.closing.Get() {
+		_ = conn.Close()
+		return
+	}
+
+	client := &EchoClient{conn: conn}
+	h.activeConn.Store(client, struct{}{})
+
+	reader := bufio.NewReader(conn)
+	for {
+		// may occur: client EOF, client timeout, server early close
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				logger.Info("connection closed")
+				h.activeConn.Delete(client)
+			} else {
+				logger.Warn(err)
+			}
+			return
+		}
+		client.waiting.Add(1)
+		b := []byte(msg)
+		_, _ = conn.Write(b)
+		client.waiting.Done()
+	}
+}
+
+func (h *EchoHandler) Close() error {
+	logger.Info("handle shutting down")
+	h.closing.Set(true)
+	h.activeConn.Range(func(key, value interface{}) bool {
+		client := value.(*EchoClient)
+		_ = client.Close()
+		return true
+	})
+	return nil
+}
---
+++ tcp/echo_test.go
@@ -0,0 +1,51 @@
+package tcp
+
+import (
+	"bufio"
+	"math/rand"
+	"net"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestListenAndServe(t *testing.T) {
+	var err error
+	closeChan := make(chan struct{})
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	addr := listener.Addr().String()
+	go ListenAndServe(listener, MakeEchoHandler(), closeChan)
+
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for i := 0; i < 10; i++ {
+		val := strconv.Itoa(rand.Int())
+		_, err = conn.Write([]byte(val + "\n"))
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		bufReader := bufio.NewReader(conn)
+		line, _, err := bufReader.ReadLine()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(line) != val {
+			t.Error("get wrong value:")
+			return
+		}
+	}
+	_ = conn.Close()
+	for i := 0; i < 10; i++ {
+		closeChan <- struct{}{}
+		time.Sleep(time.Second)
+	}
+}
